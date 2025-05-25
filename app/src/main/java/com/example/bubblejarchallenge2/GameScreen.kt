0aebb191c2126b10e647be859abc37a91f1cbf04
Jamil <jrehman2367@gmail.com>
2025-05-24 20:05:33 -0400

    Remove unused imports in GameScreen.kt file.

--- app/src/main/java/com/example/bubblejarchallenge2/GameScreen.kt
+++ app/src/main/java/com/example/bubblejarchallenge2/GameScreen.kt
@@ -1,9 +1,11 @@
 package com.example.bubblejarchallenge2
 
 import android.content.res.Configuration
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -14,15 +16,13 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.clip
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.clip
-import androidx.compose.foundation.layout.animateContentSize
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
